1818ebd2adc9188ffd06b15e64546990f2677bf8
diego andujar <diegoandujar8@gmail.com>
2020-03-15 17:19:48 -0400

    finales

--- clases_barcos.py
+++ clases_barcos.py
@@ -1,28 +1,47 @@
 
-class submarino():
+class barcos():
+    """
+    para darle direccion y posicion a los barcos
+    """
+    def __init__(self,direccion,lugar):
+        self.direccion = direccion
+        self.lugar = lugar
+
+class submarino(barcos):
+    """
+    para darle la cantidad y tamano
+    """
     cant = 4
     tamano = 1
+    caract = "los submarinos se pueden sumergir"
     def __init__(self,direccion,lugar):
-        self.lugar = lugar
-        self.direccion = direccion
+        barcos.__init__(self,direccion,lugar)
     def cant_tam(self):
-        return self.cant, self.tamano
+        return self.cant, self.tamano, self.caract
 
-class buque_grande():
+class buque_grande(barcos):
+    """
+    para darle la cantidad y tamano
+    """
     cant = 1
     tamano = 3
+    caract = "El buque pequeno se puede comunicar con tierra y otros miembros de la flota"
     def __init__(self,direccion,lugar):
-        self.lugar = lugar
-        self.direccion = direccion
+        barcos.__init__(self,direccion,lugar)
     def cant_tam(self):
-        return self.cant, self.tamano
+        return self.cant, self.tamano, self.caract
 
-class buquesito():
+class buquesito(barcos):
+    """
+    para darle la cantidad y tamano
+    """
     cant = 1
     tamano = 2
+    caract = "El buque grande permite el aterrizaje de helicopteros para el transporte de tropas"
     def __init__(self,direccion,lugar):
-        self.lugar = lugar
-        self.direccion = direccion
+        barcos.__init__(self,direccion,lugar)
     def cant_tam(self):
-        return self.cant, self.tamano
+        return self.cant, self.tamano, self.caract
+
+
 
